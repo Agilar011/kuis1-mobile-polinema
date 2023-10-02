e9f99d257d73c0caa8140ee8cc04784458777fce
Agilar011 <88954314+Agilar011@users.noreply.github.com>
2023-10-02 15:13:13 +0700

    Commits

--- README.md
+++ README.md
@@ -1,2 +1,118 @@
-A sample command-line application with an entrypoint in `bin/`, library code
-in `lib/`, and example unit test in `test/`.
+Kuis 1 Pemrograman Mobile Polinema
+
+Dikerjakan Oleh : Agilar Gumilar
+
+Kelas           :TI 3G
+
+NIM             : 2141720106
+
+Langkah 1
+
+Soal :
+
+![Alt text](image.png)
+
+Penjelasan :
+
+Pada syntak diatas merupakan perintah menampilkan sesuatu(print). Print merupakan perintah untuk menampilkan text dengan berbagai format.
+
+Langkah 2
+
+Soal :
+
+![Alt text](image-1.png)
+
+Penjelasan :
+
+![Alt text](image-2.png)
+
+Langkah 2 merupakan proses inisialisasi tipe data string dengan value = 'joko anton jodi' sehingga ketika di print sebanyak 3 kali maka tampilan seperti saya lampirkan.
+
+Langkah 3 
+
+Soal :
+
+![Alt text](image-3.png)
+
+Penjelasan :
+Langkah 3 sebenarnya mirip dengan langkah 2, namun 'final' merupakan bentuk dari string yang tidak dapat diperbarui. Sehingga setelah diinisialisai maka value tidak akan bisa dirubah.
+
+Langkah 4
+
+Soal :
+
+![Alt text](image-5.png)
+
+Penjelasan :
+
+Pada perintah diatas merupakan proses inisialisasi array1 dan array2. Disitu sayang mengganti dari final ke var karena dibawah proses inisialisasi masih ada proses perubahan value. Alasan yang sama kenapa const.
+
+Langkah 4.2
+
+Soal: 
+
+![Alt text](image-6.png)
+
+Penjelasan : 
+
+Menurut saya yang bisa dijelaskan selain pembuktian bahwa print dapat di gunakan untuk bilangan bulat, maupun bilangan desimal.
+
+Langkah 5
+
+Soal : 
+
+![Alt text](image-7.png)
+
+Penjelasan :
+
+Dari program diatas kita dapat memahami proses inisialisasi awal dan inisialisasi ulang value dari number dengan type data num yang sebenarnya tidak berbeda dari type data yang lain.
+
+Langkah 6
+
+Soal :
+
+![Alt text](image-8.png)
+
+Penjelasan :
+
+Proses diatas merupakan contoh inisialisasi variable baru dengan menggunakan variable yang ada.
+
+Langkah 7
+
+Soal : 
+
+![Alt text](image-9.png)
+
+Penjelasan :
+
+Dari perintah diatas kita belajar penggunaan \ sebagai karakter escape untuk menghindari error dalam penggunaan karakter.
+
+Langkah 8 
+
+Soal :
+
+![Alt text](image-10.png)
+
+Penjelasan :
+
+Kode diatas memberi contoh tentang macam macam cara inisialisasi untuk suatu variable.
+
+Langkah 9 
+
+Soal :
+
+![Alt text](image-11.png)
+
+Penjelasan : 
+
+Yang berbeda dari langkah 8 yaitu dalam inisialisasi name1 dimana ditambahkan " " untuk memberikan spasi. Di kasus lain untuk " " dapat diisi text apapun sebagai bentuk penambahan string ke suatu variabel.
+
+Langkah 10 
+
+Soal :
+
+![Alt text](image-12.png)
+
+Penjelasan :
+
+Di langkah no 10 mencontohkan penggunaan komentar panjang dalam dart.
--- bin/kuis1.dart
+++ bin/kuis1.dart
@@ -1,76 +1,57 @@
 import 'package:kuis1/kuis1.dart' as kuis1;
 
 void main(List<String> arguments) {
-// Tugas1
-  print('Hello world'); // You can use 'print' for debugging or simple output.
+  print('Hello world');
 
-  // Tugas2
   String name = 'joko anton jodi';
-  print(name); // You can use 'print' for debugging or simple output.
-  print(name); // You can use 'print' for debugging or simple output.
-  print(name); // You can use 'print' for debugging or simple output.
+  print(name);
+  print(name);
+  print(name);
 
-  // No need for these comments.
-
-  // // Tugas3
-  String firstName = 'joko'; // Corrected variable name to 'firstName'.
+  String firstName = 'joko';
   final lastName = 'anton jodi';
-  // print(firstName); // You can use 'print' for debugging or simple output.
-  // print(lastName); // You can use 'print' for debugging or simple output.
+  print(firstName);
+  print(lastName);
 
-  // Tugas4
   var array1 = [
     1,
     2,
-    3
-  ]; // Mengubah 'final' menjadi 'var' agar dapat dimodifikasi.
+    3,
+  ];
   List<int> array2 = [4, 5, 6];
   array1[0] = 100;
   array2[0] = 100;
-  print(array1); // Output: [100, 2, 3]
-  print(array2); // Output: [100, 5, 6]
+  print(array1);
+  print(array2);
 
-  // Tugas4
   int number1 = 100;
-  double number2 = 100.5; // Menghapus karakter yang tidak diperlukan.
+  double number2 = 100.5;
   print(number1);
   print(number2);
 
-  // Tugas5
   num number = 10;
-  print(number); // Output: 10
-  number = 10.5; // Memungkinkan num untuk berubah menjadi bilangan desimal.
-  print(number); // Output: 10.5
+  print(number);
+  number = 10.5;
+  print(number);
 
-  // Tugas6
-  print('tugas6');
-  // String firstName = 'joko';
-  // String lastName =
-  //     'anton jodi'; // Mengubah 'LastName' menjadi 'lastName' agar sesuai dengan konvensi penamaan Dart.
-  // var fullName =
-  //     '$firstName $lastName'; // Menghilangkan kurung kurawal yang tidak diperlukan.
-  // print(fullName);
+  String firstName = 'joko';
+  String lastName = 'anton jodi';
+  var fullName = '$firstName $lastName';
+  print(fullName);
 
-  // Tugas7
   var text = 'ini \'OPTIMAL\' \$sekali';
   print(text);
 
-// Tugas8
-  var name1 = firstName +
-      lastName; // Menggabungkan firstName dan lastName dengan spasi.
-  var name2 = 'joko' 'anton' 'jodi'; // Memperbaiki format penulisan.
+  var name1 = firstName + lastName;
+  var name2 = 'joko' 'anton' 'jodi';
   print(name1);
   print(name2);
 
-  // Tugas9
-  // var name1 = firstName +
-  //     " " +
-  //     lastName; // Menggabungkan firstName dan lastName dengan spasi.
-  // var name2 = 'joko' 'anton' 'jodi'; // Memperbaiki format penulisan.
-  // print(name1);
-  // print(name2);
+  var name1 = firstName + " " + lastName;
+  var name2 = 'joko' 'anton' 'jodi';
+  print(name1);
+  print(name2);
 
-  //Tugas10
   var kalimat_panjang = '''
 ini
 saya punya
